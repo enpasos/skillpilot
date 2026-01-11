b42d08c6cf74a865afe4638f89b08e2dc4d13679
enpasos <matthias.unverzagt@enpasos.com>
2026-01-11 21:16:23 +0100

    tailwind update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,6 +11,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
         injectRegister: 'inline',
