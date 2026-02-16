1e0f21a7199c5fe5ed90f50df93401923db26217
enpasos <matthias.unverzagt@enpasos.com>
2026-02-16 15:41:52 +0100

    stash local changes on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 cd "${PROJECT_ROOT}"
 
+echo "Stash local changes..."
+git stash
+
 echo "Hole Updates..."
 git pull
 
